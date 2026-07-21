8e501aaf88a2a83662b1015b6cddad5d268ca9af
sedraolapy <sedraolapy25@gmail.com>
2026-07-22 00:57:47 +0300

    admin dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('auth/admin/DAE')
             ->login()
             ->authGuard('admin')
             ->brandLogo(asset('images/logo.svg'))
